b4fe03b674b4ec0454422e9a225b0ae6b28b94f5
saiflodhi <65252652+saiflodhi@users.noreply.github.com>
2025-11-25 16:48:21 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,13 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
